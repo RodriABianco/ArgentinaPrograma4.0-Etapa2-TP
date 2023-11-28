5e0d82e0a86436c8a07bf683bd80f641559174aa
RodriABianco <130475091+RodriABianco@users.noreply.github.com>
2023-11-28 18:44:51 -0300

    v1

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,9 @@
+const Title = ({titulo}) => {
+    return (
+        <h1 className="text-5xl font-anton font-bold tracking-widest">
+            {titulo}
+        </h1>
+    );
+}
+
+export { Title }
